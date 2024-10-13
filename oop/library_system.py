83303ef07db12a304184695cf431295361d08838
davembalo <dsmbalo@gmail.com>
2024-10-13 22:05:32 +0200

    Added library system file

---
+++ oop/library_system.py
@@ -0,0 +1,37 @@
+class Book:
+    def __init__(self, title: str, author: str):
+        self.title = title
+        self.author = author
+
+
+class EBook(Book):
+    def __init__(self, title: str, author: str, file_size: int):
+        super().__init__(title, author)
+        self.file_size = file_size
+    
+
+class PrintBook(Book):
+    def __init__(self, title: str, author: str, page__count: int):
+        super().__init__(title, author)
+        self.page_count = page__count
+
+class Library:
+    def __init__(self, books = None):
+        if books is None:
+            self.books = []
+        else:
+            self.books = books
+        
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+        
+            if isinstance(book, EBook):
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}")
+            elif isinstance(book, PrintBook):
+                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
+            else:
+                print(f"Book: {book.title} by {book.author}")
+
